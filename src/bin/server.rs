f8866531b47d83aefed206864487e0c4e3dd60c3
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-09-09 20:38:11 +0200

    setup basic server listener and lib error

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,36 @@
-pub fn main() {
-    println!("hello server")
+use pigeon_rs::DEFAULT_PORT;
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{
+    fmt,
+    layer::SubscriberExt,
+    util::{SubscriberInitExt, TryInitError},
+    EnvFilter,
+};
+
+#[tokio::main]
+pub async fn main() -> pigeon_rs::Result<()> {
+    set_up_logging()?;
+
+    let port = DEFAULT_PORT;
+
+    let address = &format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(address).await?;
+
+    info!("Created listener on {}", address);
+
+    Ok(())
+}
+
+fn set_up_logging() -> Result<(), TryInitError> {
+    let filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+
+    // Use the tracing subscriber `Registry`, or any other subscriber
+    // that impls `LookupSpan`
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(fmt::Layer::default())
+        .try_init()
 }
