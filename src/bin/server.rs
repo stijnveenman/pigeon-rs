910e2278ef12290eacb9adeefd6af55c475806e0
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-09-09 20:41:14 +0200

    added clap for server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use pigeon_rs::DEFAULT_PORT;
 use tokio::net::TcpListener;
 use tracing::{info, level_filters::LevelFilter};
@@ -8,11 +9,19 @@ use tracing_subscriber::{
     EnvFilter,
 };
 
+#[derive(Parser, Debug)]
+#[command(name = "pigeon", version, author, about = "Run pegon server")]
+struct Cli {
+    #[arg(long)]
+    port: Option<u16>,
+}
+
 #[tokio::main]
 pub async fn main() -> pigeon_rs::Result<()> {
     set_up_logging()?;
 
-    let port = DEFAULT_PORT;
+    let cli = Cli::parse();
+    let port = cli.port.unwrap_or(DEFAULT_PORT);
 
     let address = &format!("127.0.0.1:{}", port);
     let listener = TcpListener::bind(address).await?;
