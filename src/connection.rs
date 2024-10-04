25e2326b9bc2ed38244cc739e4d23ad177727242
Stijn Veenman <stijn.veenman@schiphol.nl>
2024-10-04 16:06:44 +0200

    remove some comments

--- src/connection.rs
+++ src/connection.rs
@@ -16,15 +16,9 @@ pub struct Connection {
 }
 
 impl Connection {
-    /// Create a new `Connection`, backed by `socket`. Read and write buffers
-    /// are initialized.
     pub fn new(socket: TcpStream) -> Connection {
         Connection {
             stream: BufWriter::new(socket),
-            // Default to a 4KB read buffer. For the use case of mini redis,
-            // this is fine. However, real applications will want to tune this
-            // value to their specific use case. There is a high likelihood that
-            // a larger read buffer will work better.
             buffer: BytesMut::with_capacity(4 * 1024),
         }
     }
@@ -74,16 +68,8 @@ impl Connection {
     fn parse_frame(&mut self) -> crate::Result<Option<Frame>> {
         use frame::Error::Incomplete;
 
-        // Cursor is used to track the "current" location in the
-        // buffer. Cursor also implements `Buf` from the `bytes` crate
-        // which provides a number of helpful utilities for working
-        // with bytes.
         let mut buf = Cursor::new(&self.buffer[..]);
 
-        // The first step is to check if enough data has been buffered to parse
-        // a single frame. This step is usually much faster than doing a full
-        // parse of the frame, and allows us to skip allocating data structures
-        // to hold the frame data unless we know the full frame has been
         // received.
         match Frame::check(&mut buf) {
             Ok(_) => {
@@ -93,28 +79,12 @@ impl Connection {
                 // frame by checking the cursor position.
                 let len = buf.position() as usize;
 
-                // Reset the position to zero before passing the cursor to
-                // `Frame::parse`.
                 buf.set_position(0);
 
-                // Parse the frame from the buffer. This allocates the necessary
-                // structures to represent the frame and returns the frame
-                // value.
-                //
-                // If the encoded frame representation is invalid, an error is
-                // returned. This should terminate the **current** connection
-                // but should not impact any other connected client.
                 let frame = Frame::parse(&mut buf)?;
 
-                // Discard the parsed data from the read buffer.
-                //
-                // When `advance` is called on the read buffer, all of the data
-                // up to `len` is discarded. The details of how this works is
-                // left to `BytesMut`. This is often done by moving an internal
-                // cursor, but it may be done by reallocating and copying data.
                 self.buffer.advance(len);
 
-                // Return the parsed frame to the caller.
                 Ok(Some(frame))
             }
             // There is not enough data present in the read buffer to parse a
@@ -132,42 +102,21 @@ impl Connection {
         }
     }
 
-    /// Write a single `Frame` value to the underlying stream.
-    ///
-    /// The `Frame` value is written to the socket using the various `write_*`
-    /// functions provided by `AsyncWrite`. Calling these functions directly on
-    /// a `TcpStream` is **not** advised, as this will result in a large number of
-    /// syscalls. However, it is fine to call these functions on a *buffered*
-    /// write stream. The data will be written to the buffer. Once the buffer is
-    /// full, it is flushed to the underlying socket.
     pub async fn write_frame(&mut self, frame: &Frame) -> io::Result<()> {
-        // Arrays are encoded by encoding each entry. All other frame types are
-        // considered literals. For now, mini-redis is not able to encode
-        // recursive frame structures. See below for more details.
         match frame {
             Frame::Array(val) => {
-                // Encode the frame type prefix. For an array, it is `*`.
                 self.stream.write_u8(b'*').await?;
-
-                // Encode the length of the array.
                 self.write_decimal(val.len() as u64).await?;
-
-                // Iterate and encode each entry in the array.
                 for entry in &**val {
                     self.write_value(entry).await?;
                 }
             }
-            // The frame type is a literal. Encode the value directly.
             _ => self.write_value(frame).await?,
         }
 
-        // Ensure the encoded frame is written to the socket. The calls above
-        // are to the buffered stream and writes. Calling `flush` writes the
-        // remaining contents of the buffer to the socket.
         self.stream.flush().await
     }
 
-    /// Write a frame literal to the stream
     async fn write_value(&mut self, frame: &Frame) -> io::Result<()> {
         match frame {
             Frame::Simple(val) => {
@@ -195,21 +144,15 @@ impl Connection {
                 self.stream.write_all(val).await?;
                 self.stream.write_all(b"\r\n").await?;
             }
-            // Encoding an `Array` from within a value cannot be done using a
-            // recursive strategy. In general, async fns do not support
-            // recursion. Mini-redis has not needed to encode nested arrays yet,
-            // so for now it is skipped.
             Frame::Array(_val) => unreachable!(),
         }
 
         Ok(())
     }
 
-    /// Write a decimal frame to the stream
     async fn write_decimal(&mut self, val: u64) -> io::Result<()> {
         use std::io::Write;
 
-        // Convert the value to a string
         let mut buf = [0u8; 20];
         let mut buf = Cursor::new(&mut buf[..]);
         write!(&mut buf, "{}", val)?;
