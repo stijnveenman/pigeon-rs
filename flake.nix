b8ec20526170e0cb18e1e4dde3fab63f658230b5
Stijn Veenman <veenman.stijn@gmail.com>
2025-05-04 21:21:36 +0200

    add precommit hooks again

--- flake.nix
+++ flake.nix
@@ -5,12 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-pre-commit.url = "github:jmgilman/nix-pre-commit";
   };
 
   outputs = {
     nixpkgs,
     rust-overlay,
     flake-utils,
+    nix-pre-commit,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -19,9 +21,45 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        config = {
+          repos = [
+            {
+              repo = "local";
+              hooks = [
+                {
+                  id = "cargo test";
+                  entry = "cargo test";
+                  language = "system";
+                  files = "\\.rs";
+                  pass_filenames = false;
+                }
+                {
+                  id = "cargo clippy";
+                  entry = "cargo clippy";
+                  language = "system";
+                  files = "\\.rs";
+                  pass_filenames = false;
+                }
+                {
+                  id = "cargo fmt";
+                  entry = "cargo fmt";
+                  language = "system";
+                  files = "\\.rs";
+                }
+              ];
+            }
+          ];
+        };
       in {
         devShells.default = with pkgs;
           mkShell {
+            shellHook =
+              (nix-pre-commit.lib.${system}.mkConfig {
+                inherit pkgs config;
+              })
+              .shellHook;
+
             buildInputs = [
               rust-bin.stable.latest.default
               rust-analyzer
