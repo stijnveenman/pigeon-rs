39fa0452273ca0bae57432d84227e6339663eeb0
Stijn Veenman <veenman.stijn@gmail.com>
2025-05-31 22:27:21 +0200

    add record reader

--- src/segment/record_reader.rs
+++ src/segment/record_reader.rs
@@ -1,19 +1,37 @@
-use tokio::fs::{File, OpenOptions};
+use std::io::Seek;
+
+use anyhow::Error;
+use tokio::{
+    fs::{File, OpenOptions},
+    io::{AsyncRead, AsyncSeekExt, BufReader},
+    task::spawn_blocking,
+};
+
+use crate::data::record::Record;
+
+use super::record_set::RecordSet;
 
 pub struct RecordReader {
-    file: std::fs::File,
+    file: File,
 }
 
 impl RecordReader {
     pub async fn new(file_path: &str) -> Result<Self, tokio::io::Error> {
         let file = OpenOptions::new().read(true).open(file_path).await?;
 
-        Ok(Self {
-            file: file.into_std().await,
-        })
+        Ok(Self { file })
     }
 
-    pub async fn read_records_at(&self, file_offset: u32) {
-        todo!()
+    pub async fn read_records_at(
+        &mut self,
+        file_offset: u32,
+    ) -> Result<Vec<Record>, tokio::io::Error> {
+        self.file
+            .seek(std::io::SeekFrom::Start(file_offset as u64))
+            .await?;
+
+        let mut set = RecordSet::read_from(BufReader::new(&mut self.file)).await?;
+
+        set.records().await
     }
 }
