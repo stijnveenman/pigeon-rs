1266ba66779bd43a664bc1728c1dc7aa4f866044
Stijn Veenman <veenman.stijn@gmail.com>
2025-08-24 19:09:15 +0200

    implement basic record append

--- src/segment/mod.rs
+++ src/segment/mod.rs
@@ -1,6 +1,8 @@
+use std::{io::Read, path::Path};
+
 use tokio::{
-    fs::{File, OpenOptions},
-    io::{self, BufWriter},
+    fs::{create_dir_all, File, OpenOptions},
+    io::{self, AsyncWriteExt, BufWriter},
 };
 
 use crate::{config::Config, data::record::Record};
@@ -13,13 +15,17 @@ pub struct Segment {
 
 impl Segment {
     pub async fn load(config: &Config, start_offset: u64) -> Result<Self, io::Error> {
-        let path = config.segment_path(0, 0, start_offset);
+        let segment_path = config.segment_path(0, 0, start_offset);
+
+        if !Path::new(&segment_path).exists() {
+            create_dir_all(&segment_path).await?;
+        }
 
         let log_file = OpenOptions::new()
             .write(true)
             .create(true)
             .append(true)
-            .open(path)
+            .open(config.log_path(0, 0, start_offset))
             .await?;
 
         Ok(Self {
@@ -31,6 +37,66 @@ impl Segment {
     pub async fn append(&mut self, record: &Record) -> io::Result<()> {
         let mut writer = BufWriter::new(&mut self.log_file);
 
+        writer.write_u64(record.offset).await?;
+        writer.write_u64(record.timestamp.as_micros()).await?;
+
+        writer.write_u32(record.key.len() as u32).await?;
+        writer.write_all(&record.key).await?;
+
+        writer.write_u32(record.value.len() as u32).await?;
+        writer.write_all(&record.value).await?;
+
+        writer.write_u16(record.headers.len() as u16).await?;
+
+        for header in &record.headers {
+            writer.write_u32(header.key.len() as u32).await?;
+            writer.write_all(header.key.as_bytes()).await?;
+
+            writer.write_u32(header.value.len() as u32).await?;
+            writer.write_all(&header.value).await?;
+        }
+
+        writer.flush().await?;
+
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use tempfile::tempdir;
+
+    use super::Segment;
+    use crate::{
+        config::Config,
+        data::{record::Record, timestamp::Timestamp},
+    };
+
+    fn basic_record(key: &str, value: &str) -> Record {
+        Record {
+            headers: vec![],
+            offset: 0,
+            value: value.to_string().into(),
+            key: key.to_string().into(),
+            timestamp: Timestamp::now(),
+        }
+    }
+
+    #[tokio::test]
+    async fn segment_basic_read_write() {
+        let dir = tempdir().expect("failed to create tempdir");
+
+        let config = Config {
+            path: dir.path().to_str().unwrap().to_string(),
+        };
+
+        let mut segment = Segment::load(&config, 0)
+            .await
+            .expect("Failed to load segment");
+
+        segment
+            .append(&basic_record("Hello", "World"))
+            .await
+            .expect("Failed to append record");
     }
 }
