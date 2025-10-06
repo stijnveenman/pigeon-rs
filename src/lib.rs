898bbf52e97be7833b88fc028ea6b9c0c6d25e1c
Stijn Veenman <veenman.stijn@gmail.com>
2025-10-06 18:30:04 +0200

    remove old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,16 @@
 #![feature(btree_cursors)]
 
-pub mod server;
-
-pub mod client;
-
 pub mod app;
-mod cmd;
 pub mod commands;
 pub mod config;
 mod data;
 mod dur;
 pub mod http;
 mod meta;
-pub use cmd::describe_topic;
-pub use cmd::fetch;
 
 mod db;
 pub use db::Message;
 
 pub mod logging;
-
-mod connection;
-pub use connection::Connection;
-
-mod shutdown;
 
 pub const DEFAULT_PORT: u16 = 6394;
