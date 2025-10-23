3686ba1b2004feac1b21e393f880440205b50327
Stijn Veenman <veenman.stijn@gmail.com>
2025-10-23 23:01:23 +0200

    rudimentary form implemented

--- tui/src/form/mod.rs
+++ tui/src/form/mod.rs
@@ -1,9 +1,18 @@
 use std::str::FromStr;
 
-use ratatui::crossterm::event::KeyCode;
+use ratatui::{
+    crossterm::event::KeyCode,
+    layout::Rect,
+    widgets::{Block, Borders, Paragraph},
+};
 use tokio::sync::oneshot;
 
-use crate::{component::Tx, tui_event::TuiEvent, widgets::popup::Popup};
+use crate::{
+    component::Tx,
+    style::{ACTIVE_BORDER_COLOR, BORDER_STYLE},
+    tui_event::TuiEvent,
+    widgets::popup::Popup,
+};
 
 #[derive(Debug)]
 pub enum QuestionType {
@@ -32,7 +41,18 @@ impl FormPopup {
     pub fn render(&mut self, f: &mut ratatui::Frame, rect: ratatui::prelude::Rect, _active: bool) {
         let popup = Popup::new().title(self.form.title.clone());
         f.render_widget(popup.clone(), rect);
-        f.render_widget("lorom", popup.inner(rect));
+        let rect = popup.inner(rect);
+
+        let mut input_rect = rect;
+        input_rect.height = 3;
+
+        let block = Block::new()
+            .title(self.form.questions.first().unwrap().name.clone())
+            .border_style(BORDER_STYLE.fg(ACTIVE_BORDER_COLOR))
+            .borders(Borders::ALL);
+        let paragraph =
+            Paragraph::new(self.form.questions.first().unwrap().value.clone()).block(block);
+        f.render_widget(paragraph, input_rect);
     }
 
     fn finish(self) -> Option<Self> {
@@ -45,11 +65,19 @@ impl FormPopup {
         None
     }
 
-    pub fn event(self, event: TuiEvent) -> Option<Self> {
+    pub fn event(mut self, event: TuiEvent) -> Option<Self> {
         match event {
             TuiEvent::KeyPress(key) => match key.code {
-                KeyCode::Char('q') | KeyCode::Esc => self.close(),
+                KeyCode::Esc => self.close(),
                 KeyCode::Enter => self.finish(),
+                KeyCode::Char(c) => {
+                    self.form.questions.first_mut().unwrap().value.push(c);
+                    Some(self)
+                }
+                KeyCode::Backspace => {
+                    self.form.questions.first_mut().unwrap().value.pop();
+                    Some(self)
+                }
                 _ => Some(self),
             },
             _ => Some(self),
