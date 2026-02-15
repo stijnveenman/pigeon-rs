b8a9c952a85c0611b52ac26d1b1ed08c3902471d
Stijn Veenman <veenman.stijn@gmail.com>
2026-02-15 20:49:57 +0100

    add parsing failure unit tests

--- pigeon-core/src/error.rs
+++ pigeon-core/src/error.rs
@@ -1,7 +1,9 @@
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq, Eq)]
 pub enum PError {
+    #[error("Offset cannot be added to index")]
+    IndexOffsetNotAllowed,
     #[error("Failed to parse Index")]
     IndexParseFailed,
 }
--- pigeon-server/src/dur/index.rs
+++ pigeon-server/src/dur/index.rs
@@ -7,10 +7,14 @@ use pigeon_core::PError;
 pub struct Index(BTreeMap<u64, u64>);
 
 impl Index {
-    pub fn insert(&mut self, offset: u64, byte_offset: u64) {
-        assert!(self.0.last_entry().is_none_or(|v| offset > *v.key()));
+    pub fn insert(&mut self, offset: u64, byte_offset: u64) -> Result<(), PError> {
+        if self.0.last_entry().is_some_and(|v| offset <= *v.key()) {
+            return Err(PError::IndexOffsetNotAllowed);
+        }
 
         self.0.insert(offset, byte_offset);
+
+        Ok(())
     }
 
     pub fn get(&self, offset: u64) -> Option<&u64> {
@@ -27,7 +31,9 @@ impl TryFrom<&[u8]> for Index {
             let offset = value.try_get_u64().map_err(|_| PError::IndexParseFailed)?;
             let byte_offset = value.try_get_u64().map_err(|_| PError::IndexParseFailed)?;
 
-            index.insert(offset, byte_offset);
+            index
+                .insert(offset, byte_offset)
+                .map_err(|_| PError::IndexParseFailed)?;
         }
 
         Ok(index)
@@ -36,26 +42,27 @@ impl TryFrom<&[u8]> for Index {
 
 #[cfg(test)]
 mod test {
+    use pigeon_core::PError;
+
     use crate::dur::index::Index;
 
     #[test]
     fn insert_and_get() {
         let mut index = Index::default();
 
-        index.insert(0, 1);
-        index.insert(1, 2);
+        index.insert(0, 1).unwrap();
+        index.insert(1, 2).unwrap();
 
         assert_eq!(index.get(0), Some(&1));
         assert_eq!(index.get(1), Some(&2));
     }
 
     #[test]
-    #[should_panic]
     fn can_only_insert_at_the_end() {
         let mut index = Index::default();
 
-        index.insert(10, 10);
-        index.insert(0, 0);
+        assert_eq!(index.insert(10, 10), Ok(()));
+        assert_eq!(index.insert(0, 0), Err(PError::IndexOffsetNotAllowed));
     }
 
     #[test]
@@ -77,5 +84,40 @@ mod test {
         assert_eq!(index.get(0), Some(&0));
         assert_eq!(index.get(1), Some(&5));
         assert_eq!(index.get(3), Some(&10));
+    }
+
+    #[test]
+    fn error_on_incomplete_index() {
+        let mut bytes: Vec<u8> = [
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01],
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05],
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03],
+        ]
+        .into_iter()
+        .flatten()
+        .collect();
+        bytes.append(&mut [0x00, 0x00, 0x00, 0x00].to_vec());
+
+        let index = Index::try_from(bytes.as_slice());
+        assert_eq!(index.err(), Some(PError::IndexParseFailed));
+    }
+
+    #[test]
+    fn error_on_misordered_index() {
+        let mut bytes: Vec<u8> = [
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01],
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05],
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+        ]
+        .into_iter()
+        .flatten()
+        .collect();
+        bytes.append(&mut [0x00, 0x00, 0x00, 0x00].to_vec());
+
+        let index = Index::try_from(bytes.as_slice());
+        assert_eq!(index.err(), Some(PError::IndexParseFailed));
     }
 }
