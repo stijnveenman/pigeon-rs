9f217fa92aba425a2c564823cf6815c62a9499af
Stijn Veenman <veenman.stijn@gmail.com>
2026-02-18 18:21:49 +0100

    add another log unit test

--- pigeon-server/src/dur/log/mod.rs
+++ pigeon-server/src/dur/log/mod.rs
@@ -30,4 +30,33 @@ mod test {
 
         assert_eq!(record, read_record);
     }
+
+    #[tokio::test]
+    async fn rw_log_records_with_end_offset() {
+        let dir = tempdir().unwrap();
+        let base_dir = dir.path().to_str().unwrap();
+
+        let mut writer = LogWriter::new(base_dir, 0);
+        let record = Record {
+            offset: 1,
+            key: "hello".as_bytes().to_vec(),
+            value: "world".as_bytes().to_vec(),
+        };
+
+        let end_offset = writer.append(&record).await.unwrap();
+        writer
+            .append(&Record {
+                offset: 2,
+                value: Vec::new(),
+                key: Vec::new(),
+            })
+            .await
+            .unwrap();
+        drop(writer);
+
+        let reader = LogReader::open(base_dir, 0).await.unwrap();
+        let read_record = reader.read_record(0, Some(end_offset)).await.unwrap();
+
+        assert_eq!(record, read_record);
+    }
 }
