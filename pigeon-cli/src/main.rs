e3f2768f101acbe17ab21b450d4d60cbad1dd70f
Stijn Veenman <veenman.stijn@gmail.com>
2026-03-13 17:13:36 +0100

    basic http sdk setup

---
+++ pigeon-cli/src/main.rs
@@ -0,0 +1,8 @@
+use pigeon_sdk::pigeon_sdk::PigeonSdk;
+
+#[tokio::main]
+async fn main() {
+    let sdk = PigeonSdk::new("http://localhost:4111");
+
+    sdk.read_record("foo", 0, 5).await;
+}
--- pigeon-sdk/src/pigeon_sdk.rs
+++ pigeon-sdk/src/pigeon_sdk.rs
@@ -10,6 +10,10 @@ impl PigeonSdk {
         Request::new(method, self.base_url.join(path).unwrap())
     }
 
+    fn get(&self, path: &str) -> Request {
+        self.request(Method::GET, path)
+    }
+
     pub fn new(base_url: impl Into<String>) -> Self {
         let mut base_url = base_url.into();
         if !base_url.ends_with("/") {
@@ -23,5 +27,16 @@ impl PigeonSdk {
                 .build()
                 .unwrap(),
         }
+    }
+
+    pub async fn read_record(&self, topic_name: &str, partition_id: u64, offset: u64) {
+        let response = self
+            .client
+            .execute(self.get(&format!("/topics/{topic_name}/{partition_id}/{offset}")))
+            .await
+            .unwrap();
+
+        dbg!(&response);
+        dbg!(response.text().await);
     }
 }
